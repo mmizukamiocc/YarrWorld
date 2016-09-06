f51d0d98cda1729ea8535b0363f28ac7f552ca7d
mmizukami <jplockongn006@gmail.com>
2016-09-06 16:11:30 -0700

    recover some of files

--- app/src/main/java/edu/orangecoastcollege/cs273/mmizukami/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/mmizukami/yarrworld/MainActivity.java
@@ -1,13 +1,37 @@
 package edu.orangecoastcollege.cs273.mmizukami.yarrworld;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button speakButton;
+    private Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        speakButton = (Button) findViewById(R.id.speakButton);
+
+    // Wire up the speak button to perform an onClick event
+        speakButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Use the static reference to "Toast" to display the message:
+                        // Toast = vanishes after set amount of time
+                        // Dialog = requires user input
+                        Toast.makeText(context, R.string.toast_text,Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
     }
 }
